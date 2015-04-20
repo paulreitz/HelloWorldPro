4ce6effdb718e420544cc2c5e5b2fc9023686f07
Paul Reitz <avenose@gmail.com>
2015-04-19 19:10:58 -0700

    core: Added HelloView class
    Added the main view class that will display the "Hello, World!" message.

---
+++ src/hello/views/HelloView.java
@@ -0,0 +1,49 @@
+package hello.views;
+
+import hello.model.HelloVO;
+
+public class HelloView implements IViewRenderer
+{
+	private HelloVO messageVO;
+	
+	public HelloView()
+	{
+		messageVO = new HelloVO();
+	}
+	
+	public HelloView(String message)
+	{
+		messageVO = new HelloVO(message);
+	}
+	
+	public HelloView(HelloVO mVo)
+	{
+		messageVO = mVo;
+	}
+
+	public HelloVO getMessageVO()
+	{
+		return messageVO;
+	}
+
+	public void setMessageVO(HelloVO value)
+	{
+		messageVO = value;
+	}
+	
+	public String getMessage()
+	{
+		return messageVO.getMessage();
+	}
+	
+	public void setMessage(String value)
+	{
+		messageVO.setMessage(value);
+	}
+
+	public void render()
+	{
+		
+	}
+
+}
